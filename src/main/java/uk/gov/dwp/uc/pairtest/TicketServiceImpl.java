b30e7d532df96774c9bc195bde999abf8cc764d4
nitin <nitinnannapaneni@gmail.com>
2025-09-20 14:30:46 +0100

    Added account and ticket request validation in the TicketServiceImpl

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -4,13 +4,31 @@ import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
-    /**
-     * Should only have private methods other than the one below.
-     */
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        checkAccountId(accountId);
+        checkTicketRequests(ticketTypeRequests);
+    }
 
+    // Making sure account ID is valid
+    private void checkAccountId(Long accountId) {
+        if (accountId == null || accountId <= 0) {
+            throw new InvalidPurchaseException();
+        }
+    }
+
+    // Making sure ticket requests are valid
+    private void checkTicketRequests(TicketTypeRequest... requests) {
+        if (requests == null || requests.length == 0) {
+            throw new InvalidPurchaseException();
+        }
+        
+        for (TicketTypeRequest request : requests) {
+            if (request.getNoOfTickets() <= 0) {
+                throw new InvalidPurchaseException();
+            }
+        }
     }
 
 }
